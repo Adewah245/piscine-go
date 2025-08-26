9378abbaf374c8d67b34fd5852175bef08d0f328
James Adah <inalegwujamesadah09@gmail.com>
2025-08-26 15:46:28 +0100

    myphones commit

--- main.go
+++ main.go
@@ -6,6 +6,24 @@ import (
 )
 
 func main() {
-	fmt.Println("how are you")
-	fmt.Println(t.Now())
+	var myPhones string
+	var Battery int
+	var Product string
+	var price float64
+	var purchase bool
+
+	// declaring my variables
+	myPhones = "android"
+	Battery = 75
+	Product = "tecno"
+	price = 34.000
+	purchase = true
+
+	// printing my phone details
+	fmt.Println("Product:", Product)
+	fmt.Println("Phone Type:", myPhones)
+	fmt.Println("Battery Percentage:", Battery, "%")
+	fmt.Println("Price: $", price)
+	fmt.Println("Purchased:", purchase)
+	fmt.Println("Current Time:", t.Now())
 }
